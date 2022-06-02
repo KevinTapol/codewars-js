acb83a5167c285551f703fa8c26c99574e04e173
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2022-06-02 03:07:36 -0500

    push an object into array

---
+++ codewars015.js
@@ -0,0 +1,7 @@
+// Parameters or Edge cases:
+// Return: an array with an object 
+// Examples:
+// Psuedo code: declare an empty array // assign an object and put it into the empty array with .unshift() add at the front of the array or .push() add at the end of the array
+items = [], a = {a: 'b', c: 'd'}
+items.push(a)
+//items.unshift(a) also works since the array is empty adding to the end or the beginning still becomes the only element in the array
