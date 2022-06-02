c00fe1d08aedbfaf5792ba52cbde6550e2ab5004
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2022-06-02 03:09:46 -0500

    add element into array DO NOT return

---
+++ codewars018.js
@@ -0,0 +1,6 @@
+//Basic Training: Add item to an Array Add the value "codewars" to the websites array.
+// Parameters or Edge cases: var websites = [] has already been assigned 
+// Return: No Return
+// Examples:
+// Psuedo code: declare emtpy array assign string codewars .push() or .unshift() the string
+websites.push("codewars")
