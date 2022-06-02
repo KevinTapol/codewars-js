e5b81a802fd976ba30627c4ae9d3e0ccfd9e72fc
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2022-06-02 03:13:35 -0500

    obj input return string with template literals

---
+++ codewars024.js
@@ -0,0 +1,11 @@
+// Parameters or Edge cases: function accepts 1 parameter object
+// Return: a string with object
+// Examples: {name:"dog",legs:4,color:"white"}
+            //and return a string like this:
+            //"This white dog has 4 legs."
+// Psuedo code: 
+
+function animal(obj){
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
+  
