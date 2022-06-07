12a4f7290ff603841efddc7e096983118bd78faf
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2022-06-07 04:06:12 -0500

    num to string

---
+++ codewars030.js
@@ -0,0 +1,11 @@
+// Parameters or Edge cases:
+// Return: a string
+// Examples: 123 --> "123"
+// Psuedo code: create a function with a string input .toString() and return
+
+function numberToString(num){
+    return num.toString()
+}
+
+//return String(num)
+//return ''+num
