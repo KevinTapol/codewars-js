0f1f4afd255b8745e030b03fa1588e6e8e2c4610
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2022-06-08 06:46:45 -0500

    convert boolean values to string

---
+++ codewars031.js
@@ -0,0 +1,10 @@
+// Parameters or Edge cases:
+// Return: string 'yes' if parameter is boolean 'no' for false
+// Examples:
+// Psuedo code: ternary true yes false no
+
+
+
+function boolToWord( bool ){
+    return bool ? 'Yes':'No';
+  }
