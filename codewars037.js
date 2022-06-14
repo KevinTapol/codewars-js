b493c6a37658b7ef5d4d9610a2c69f0bef58f88d
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2022-06-14 03:24:39 -0500

    return greeting with user input

---
+++ codewars037.js
@@ -0,0 +1,15 @@
+// Parameters or Edge cases: space sensative 
+// Return: "Hello, <name> how are you doing today?"
+// Examples:
+// Psuedo code: 
+
+//my answer
+function greet(name){
+    return "Hello, "+name+" how are you doing today?"
+  }
+
+//best practices
+function greet(name){
+    return `Hello, ${name} how are you doing today?`;
+  }
+  
