24f45daacf496e5341cad66f688dd89d510bcd3f
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2022-06-28 06:30:21 -0500

    join strings w/o using new ES6 string addition

---
+++ codewars051.js
@@ -0,0 +1,10 @@
+// Parameters or Edge cases: + , .concat() & .join() will not be allowed in this exercise
+                            //If one of the arguments is a number your code must coerce it into being a string.
+// Return: a joined string
+// Examples:
+// Psuedo code: use template literals
+
+//my answer also best practices
+function joinStrings(string1, string2){
+    return `${string1} ${string2}`
+ }
