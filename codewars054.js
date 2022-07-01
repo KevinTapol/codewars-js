3d03f4efc55b54d898dbee97a9aa3a7ecbeb6f1a
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2022-07-01 06:09:38 -0500

    find the opposite

---
+++ codewars054.js
@@ -0,0 +1,18 @@
+// Parameters or Edge cases:
+// Return: the opposite of a given integer or floating point number
+// Examples: 1: -1
+            //14: -14
+            //-34: 34
+
+// Psuedo code: multiply by -1
+
+//my answer
+function opposite(number) {
+    return number*(-1)
+  }
+
+//best practices
+function opposite(number) {
+    return(-number);
+  }
+  
