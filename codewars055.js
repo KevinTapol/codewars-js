1699c8eddf06470ac00e60e6cd858c38fbd91687
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2022-07-02 06:05:24 -0500

    return a neg num

--- codewars055.js
+++ codewars055.js
@@ -4,4 +4,13 @@
             //makeNegative(-5);   // return -5
             //makeNegative(0);    // return 0
             //makeNegative(0.12); // return -0.12
-// Psuedo code:
+// Psuedo code: arrow function ternary num < 0 ? return the num : multiply by -1 and return the result
+
+//my answer
+const makeNegative = (num) => num < 0 ? num : -num
+
+//best practices
+function makeNegative(num) {
+    return -Math.abs(num);
+  }
+  
