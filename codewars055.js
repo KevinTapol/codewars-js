9777de17428786be7583bcf4b48f71498534d0d6
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2022-07-02 05:38:21 -0500

    return a negative number

---
+++ codewars055.js
@@ -0,0 +1,7 @@
+// Parameters or Edge cases: given a number can be positive or negative
+// Return: a negative number
+// Examples: makeNegative(1);    // return -1
+            //makeNegative(-5);   // return -5
+            //makeNegative(0);    // return 0
+            //makeNegative(0.12); // return -0.12
+// Psuedo code:
