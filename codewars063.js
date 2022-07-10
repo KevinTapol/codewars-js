98795c69ff1fb72eed5a64db5fa8aac38fb3c691
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2022-07-10 15:38:59 -0500

    print hello world!

---
+++ codewars063.js
@@ -0,0 +1,11 @@
+// Parameters or Edge cases:
+// Return: hello world
+// Examples:
+// Psuedo code: create a function that when called returns the string hello world!
+
+//my answer and best practices
+function greet(){
+    return 'hello world!'
+}
+
+//other answers had elaborate ascii art
