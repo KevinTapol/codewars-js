0947e98a56ff098e82e5f5837e9949cbe3371810
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2022-07-14 09:22:02 -0500

    code as fast as you can

---
+++ codewars067.js
@@ -0,0 +1,15 @@
+// Parameters or Edge cases: Code As Fast As You Can!!
+// Return: integer * 2
+// Examples:
+// Psuedo code: return paramater * 2
+
+
+//my answer and best practices
+function doubleInteger(i) {
+    return i*2;
+  }
+
+//most clever
+function doubleInteger(i) {
+    return i << 1;
+  }
