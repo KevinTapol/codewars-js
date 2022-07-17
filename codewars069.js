3e94d88535dd2c5714a11a583aae65eb8978d3ca
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2022-07-16 21:19:50 -0500

    double values in an array

---
+++ codewars069.js
@@ -0,0 +1,9 @@
+// Parameters or Edge cases: input array elements are integers
+// Return: an array of values doubled
+// Examples: [1, 2, 3] --> [2, 4, 6]
+// Psuedo code: .map() multiplying by 2
+
+//my answer best practices most clever
+function maps(x){
+   return x.map(e => e*2)
+}
