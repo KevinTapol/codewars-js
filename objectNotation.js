15a198246a07f3fd889bbffa6070a40b3c9225cf
Kevin Tapolcsanyi <104818933+KevinTapol@users.noreply.github.com>
2022-09-29 20:52:26 -0500

    Delete objectNotation.js

--- objectNotation.js
+++
@@ -1,9 +0,0 @@
-let stopwatch = {}
-
-stopwatch.currentTime = 12
-
-stopwatch.tellTime = function(time){
-  console.log(`The current time is ${time}.`)
-}
-
-stopwatch.tellTime(stopwatch.currentTime)
