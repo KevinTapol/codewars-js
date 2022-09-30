1e06902bef1299a5d4fa63086d9be29813ad5aed
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2022-09-29 20:50:36 -0500

    updated readme

---
+++ pmg.js
@@ -0,0 +1,27 @@
+let numbers = ["one","two","three","four"];
+for(let x = 0; x < word(numbers); x++){
+  const item = numbers[x];
+  if(word(item) !== true){
+    console.log(word(item));
+  }
+  else{
+    console.log("five")
+  }
+}
+
+function word(i){
+  return i.length;
+}
+
+
+function elementLength(){
+	let numbers = ["one", "two", "three", "four"];
+	for(let i = 0, i <= numbers.length; i++){
+		console.log(numbers[i].length
+}
+elementsLength()
+
+
+const elementLength = (array) => array.forEach(word => console.log(word.length))
+let numbers = ["one", "two", "three", "four"];
+
