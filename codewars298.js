c9c0568851dbb9308840047ff5820f495a8be816
KevinTapol <kevintapolcanyi@gmail.com>
2024-06-15 23:49:07 -0500

    array methods of sorting strings

---
+++ codewars298.js
@@ -0,0 +1,20 @@
+/*
+Sort by Last Char
+Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+If two words have the same last letter, the returned array should show them in the order they appeared in the given string.
+
+All inputs will be valid.
+Parameters or Edge Cases:
+Return:
+Examples:
+   assert.deepEqual(last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']);
+    assert.deepEqual(last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']); 
+    assert.deepEqual(last('take me to semynak'), ['take', 'me', 'semynak', 'to']);  
+Pseudocode:
+*/
+
+// My Answer
+function last(x){
+
+}
