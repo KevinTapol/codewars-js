d716b2cc2f9c57c42c7955f279b4c267d4a3f6f6
KevinTapol <kevintapolcanyi@gmail.com>
2024-06-20 22:20:19 -0500

    setup

---
+++ codewars302.js
@@ -0,0 +1,19 @@
+/*
+How many are smaller than me?
+Write a function that given, an array arr, returns an array containing at each index i the amount of numbers that are smaller than arr[i] to the right.
+
+Parameters or Edge Cases:
+    inputs will be an array of integers
+Return:
+
+Examples:
+* Input [5, 4, 3, 2, 1] => Output [4, 3, 2, 1, 0]
+* Input [1, 2, 0] => Output [1, 1, 0]
+
+Pseudocode:
+*/
+
+// My Answer
+function smaller(nums) {
+
+}
