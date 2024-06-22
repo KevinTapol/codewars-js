380546b35d61ef2217d51b77b68977057b80dec5
KevinTapol <kevintapolcanyi@gmail.com>
2024-06-21 23:05:21 -0500

    psuedocode

--- codewars302.js
+++ codewars302.js
@@ -4,16 +4,32 @@ Write a function that given, an array arr, returns an array containing at each i
 
 Parameters or Edge Cases:
     inputs will be an array of integers
+    integers can be positive or negative
 Return:
-
+    the total number of elements in the array as we continue through the array that are greater than the current element
 Examples:
-* Input [5, 4, 3, 2, 1] => Output [4, 3, 2, 1, 0]
-* Input [1, 2, 0] => Output [1, 1, 0]
+    [5, 4, 3, 2, 1]  -->  [4, 3, 2, 1, 0]
+    [1, 2, 3]  -->  [0, 0, 0]
+    [1, 2, 0]  -->  [1, 1, 0]
+    [1, 2, 1]  -->  [0, 1, 0]
+    [1, 1, -1, 0, 0]  -->  [3, 3, 0, 0, 0]
+    [5, 4, 7, 9, 2, 4, 4, 5, 6]  -->  [4, 1, 5, 5, 0, 0, 0, 0, 0]
 
 Pseudocode:
+    declare an empty array named result
+    iterate through the input array declaring a local variable count set to 0 and for each current element count the number of elements proceeding that are less than the current element and add 1 to the total count for each element
+    push the total count to the empty array result
+    return the array result
 */
 
 // My Answer
 function smaller(nums) {
-
+    let result = []
+    for(let i = 0; i <= nums.length; i++){
+        let count = 0
+        if(nums[i]){
+            count +=1
+        }result.push(count)
+    }
+    return result
 }
