b0fd2c8982be54e486456fc9e3e36b9c2cac4602
KevinTapol <kevintapolcanyi@gmail.com>
2024-06-24 23:33:37 -0500

    nested for loop to map slice filter

--- codewars302.js
+++ codewars302.js
@@ -46,3 +46,32 @@ console.log(smaller([5, 4, 3, 2, 1])) // [4, 3, 2, 1, 0]
 // Best Practices and Most Clever
 // arrow fn using .map() with a nested .slice().filter().length
 const smaller = nums => nums.map((x, i) => nums.slice(i).filter(y => x > y).length);
+
+// using .slice() with .reduce()
+function smaller(nums) {
+    return nums.map((num,index) => {
+      return nums
+        .slice(index + 1)
+        .reduce((acc, num2) => {
+          if(num > num2) acc +=1
+          return acc
+        } ,0)
+    })
+}
+
+// same as above but using .map() to keep input integrity
+function smaller(numbers) {
+    return numbers.map((x, i) => 
+      numbers.slice(i + 1).reduce((count, y) => 
+        count + (x > y), 0));
+  }
+
+//   .map() .slice() .reduce() nested ternary
+function smaller(nums) {
+    return nums.map((v, i, arr) => arr.slice(i).reduce((sum, v2) => v2 < v ? sum + 1 : sum, 0))
+  }
+
+// map slice filter
+function smaller(nums) {
+    return nums.map((e,i) => nums.slice(i).filter(x => e>x).length);
+  }
