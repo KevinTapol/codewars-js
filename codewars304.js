b2b1326c3b58a7b53ed146aa17172a912c39642f
KevinTapol <kevintapolcanyi@gmail.com>
2024-06-27 23:40:51 -0500

    regex .replace() delimiter String() .toString() Number()

--- codewars304.js
+++ codewars304.js
@@ -33,7 +33,67 @@ function insertDash(num) {
     return result.join('')
 }
 
+// My Answer refactored to ternary instead of if else statement
+function insertDash(num) {
+    let result = []
+    let arr = String(num).split('')
+    for(let i = 0; i < arr.length; i++){
+        arr[i] % 2 != 0 && arr[i + 1] %2 != 0 && i != arr.length -1 ? result.push(arr[i] + "-") : result.push(arr[i]) 
+    }
+    return result.join('')
+}
+
+// My Answer Refactored using array methods
+function insertDash(num) {
+    let arr = String(num).split('')
+    return arr.map((e, i) => arr[i] % 2 != 0 && arr[i + 1] %2 != 0 && i != arr.length -1? e = arr[i] + "-" : e = arr[i]).join('')
+}
+
+// My Answer array methods refactored to default declared params of passed in arguments
+function insertDash(num, arr = String(num).split('')) {
+    return arr.map((e, i) => arr[i] % 2 != 0 && arr[i + 1] %2 != 0 && i != arr.length -1? e = arr[i] + "-" : e = arr[i]).join('')
+}
+
+
+// My Answer One liner arrow fn
+const insertDash = (num, arr = String(num).split('')) => arr.map((e, i) => arr[i] % 2 != 0 && arr[i + 1] %2 != 0 && i != arr.length -1? e = arr[i] + "-" : e = arr[i]).join('')
+
 console.log(insertDash(454793)) // '4547-9-3'
 console.log(insertDash(123456)) // '123456'
 console.log(insertDash(1003567)) // '1003-567'
  
+// Best Practice and Most Clever
+// using regex with .replace specifying odd integer be separated with "-"
+function insertDash(num) {
+    return num.toString().replace(/[13579](?=[13579])/g, "$&-");
+}
+
+// Same as above but using String() instead of .toString()
+function insertDash(num) {
+    return String(num).replace(/([13579])(?=[13579])/g, '$1-');
+ }
+
+// Similar to my unrefactored for loop but cleaner conditional statements than mine
+function insertDash(num) {
+    //code me
+  num = num.toString().split('')
+   
+   for(let i = 0; i < num.length; i++){
+     if (num[i]%2 > 0 && num[i +1]%2 > 0){
+       num[i] = num[i] + '-'
+     }
+   }
+  
+   return num.join('')
+   
+ }
+
+//  Very Clever using .reduce() with a conditional
+function insertDash(num) {
+    return num.toString().split('').reduce((acc, c, i, arr) => {
+      if (i > 0 && Number(arr[i]) % 2 !== 0 && Number(arr[i - 1]) % 2 !== 0)
+        acc = acc + '-';
+      
+      return acc + c;
+    }, '');
+ }
