cee5ebe3bcdf67c6adb371eed5712c4e9d078f70
KevinTapol <kevintapolcanyi@gmail.com>
2024-07-06 17:11:52 -0500

    setup

--- codewars306.js
+++ codewars306.js
@@ -53,7 +53,15 @@ Note : The last element 2 is greater than the sum of its right elements (abstrac
 Parameters or Edge Cases:
 
 Return:
+
 Examples:
+    [1,2,3,4,0]  --> [4]
+    [16,17,4,3,5,2]  --> [17,5,2]
+    [-1,-29,-26,-2]  --> [-1]
+    [-36,-12,-27]  -->  [-36,-12]
+    [5,-2,2]  --> [5,2]
+    [0,-1,-29,3,2]  -->  [0,-1,3,2]
+
 Pseudocode:
 */
 
