0db5dc2abdca48d5e83683c2911eb49bc3c40e85
KevinTapol <kevintapolcanyi@gmail.com>
2024-07-22 20:52:59 -0500

    .reduce((a,c)=>{if return a},0)return result

--- codewars317.js
+++ codewars317.js
@@ -12,7 +12,6 @@ Examples:
     [12, 0, 0, 1], 12 --> 3
 
 Pseudocode:
-    sort the input array
     declare a global variable int named count and set it equal to 0
     iterate through the sorted array counting the number of elements summed while not exceeding the 2nd integer input adding 1 to count each time
     return the count
@@ -22,15 +21,41 @@ Pseudocode:
 function save(sizes, hd) {
     let count = 0
     let total = 0
-    let arr = sizes.sort((a, b) => a - b)
     for(let i =0; i <= sizes.length; i++){
-        // console.log(total)
-        // console.log(sizes[i])
         total += sizes[i]
         if(total <= hd){
             count++   
+        } else if (total > hd){
+            return count
         }
     }return count
 }
 
 console.log(save([4,4,4,3,3], 12)) // 3
+
+// Best Practices and Most Clever
+// while loop manipulating input hd
+function save(sizes, hd) {
+    let i = -1;
+    while (hd >=0) {
+      hd -= sizes.shift();
+      i++;
+    }
+    return i;
+}
+
+// IMO Most Clever to use .reduce({conditional},0) 
+function save(sizes, hd) {
+    let result = 0;
+    sizes.reduce((acc, curr) => {
+      if ((acc += curr) <= hd) result += 1;
+      return acc;
+    }, 0);
+    return result;
+  }
+
+// Confusing at first glance but incredibly clever to create an array with the 2nd parameter including a spread operator and finally recursion
+save=([a,...b],h)=>h-a>=0?1+save(b,h-a):0
+
+// Very Clever creating a copy of the input array where each element has .reduce() applied to it with conditional <= hd and finally .filter().length
+const save = ($, hd) => $.map((_, i)=> $.slice(0, i+1).reduce((a,b)=> a+b, 0) <= hd ).filter(el => el == true).length
