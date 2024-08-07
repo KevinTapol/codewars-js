502146be046313feaa3330df00ccfa05fb8400bc
KevinTapol <kevintapolcanyi@gmail.com>
2024-08-06 21:13:22 -0500

    recurssion .flat(Infinity) strings inside nested object values further nested and potential nested arrays

--- codewars325.js
+++ codewars325.js
@@ -25,28 +25,56 @@ Pseudocode
         if the type of the current element value is a string then push it to the array named result
         if the current element value is an object and not null, undefined or empty then create a local array of the values of the current element value and iterate through the local current element value
             if the nested for loop element object value type is a string then push the current element to the global array result
+    outside of both of the for loops return the length of the array of strings
 
 */
 
 // My Answer
 function strCount(obj){
-    let result = [];
-    let arrOfVals = Object.values(obj);
-    let flatArr = arrOfVals.flat(Infinity);
+    let arrOfStrings = [];
+    let arrOfValues = Object.values(obj);
+    let flatArr = arrOfValues.flat(Infinity);
 
     for (let i = 0; i <= flatArr.length; i++){
         if (typeof flatArr[i] === "string") {
-            result.push(flatArr[i]);
+            arrOfStrings.push(flatArr[i]);
 
         } else if (typeof flatArr[i] === "object" && flatArr[i] !== null) {
-            let newArr = Object.values(flatArr[i]);
+            let localArr = Object.values(flatArr[i]);
 
-            for (let j = 0; j <= newArr.length; j++) {
-                if (typeof newArr[j] === "string") {
-                    result.push(newArr[j]);
+            for (let j = 0; j <= localArr.length; j++) {
+                if (typeof localArr[j] === "string") {
+                    arrOfStrings.push(localArr[j]);
                 } 
             }
         }
-    }return result.length;
+    }return arrOfStrings.length;
 }
 
+// Best Practices and Most Clever
+// using recursion for nested objects instead of infinitely flattened array of values
+function strCount(obj){
+    let count = 0;
+    for (key in obj) {
+      if (typeof obj[key] == 'string') count++;
+      if (typeof obj[key] == 'object') count += strCount(obj[key]);
+    }
+    return count;
+}
+
+// one liner with recursion inside of .reduce()
+const strCount = obj => typeof obj !== `string` ? Object.values(obj || {}).reduce((pre, val) => pre + strCount(val), 0) : 1;
+
+// Very Clever to use JSON.stringify() for the object 
+// IMO should be Most Clever and maybe even Best Practices
+function strCount(obj){
+    let temp = 0;
+    JSON.stringify(obj,(key,value)=>{
+        if (typeof value === 'string') {
+            temp++
+        }
+        return value;
+    })
+
+    return temp
+}
