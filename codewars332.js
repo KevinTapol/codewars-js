3eeb03f6a2dafb78add93dca3bc529dfa0973584
KevinTapol <kevintapolcanyi@gmail.com>
2024-08-16 22:51:05 -0500

    setup 3 inputs int output a string of ints with comma as a delimiter

---
+++ codewars332.js
@@ -0,0 +1,24 @@
+/*
+Arithmetic progression
+In your class, you have started lessons about arithmetic progression. Since you are also a programmer, you have decided to write a function that will return the first n elements of the sequence with the given common difference d and first element a. Note that the difference may be zero!
+
+The result should be a string of numbers, separated by comma and space.
+
+Example
+# first element: 1, difference: 2, how many: 5
+arithmetic_sequence_elements(1, 2, 5) == "1, 3, 5, 7, 9"
+Parameters or Edge Cases:
+Return:
+Examples:
+    1, 2, 5 --> "1, 3, 5, 7, 9"
+    1, 0, 5 --> "1, 1, 1, 1, 1"
+    1, -3, 10 --> "1, -2, -5, -8, -11, -14, -17, -20, -23, -26"
+    100, -10, 10 --> "100, 90, 80, 70, 60, 50, 40, 30, 20, 10"
+
+Pseudocode:
+*/
+
+// My Answer
+function arithmeticSequenceElements(a, d, n) {
+	return "";
+}
