f87d60f1920eb0a462b83c9368b6af57808f337d
KevinTapol <kevintapolcanyi@gmail.com>
2024-08-23 23:30:52 -0500

    itterate through array of objects selecting specific key values

---
+++ codewars335.js
@@ -0,0 +1,22 @@
+/*
+Pluck
+
+Parameters or Edge Cases:
+    inputs will be an array of objects and a single char representing a property key name to identify in a key value pair
+    if an object is missing the property key name then return undefined
+
+Return:
+    return an array of values that exists in the 1st input object for the property name key 2nd input 
+
+Examples:
+[{a:1}, {a:2}], 'a' -> [1,2]
+[{a:1, b:3}, {a:2}], 'b' --> [3, undefined]
+
+Pseudocode:
+    iterate through the array
+*/
+
+// My Answer
+function pluck(objs, name) {
+
+}
