259bc5e2453f32c3a30c9cd13918c78285bfb2ba
KevinTapol <kevintapolcanyi@gmail.com>
2024-08-27 20:21:00 -0500

    Gauss Theorem n*(n+1)/2 n is input integer array length

--- methodsToStudy.js
+++ methodsToStudy.js
@@ -31,3 +31,18 @@ Take in an object of key value pairs and return a 2d array where each element is
 const convertHashToArray = o => Object.entries(o)
 
 console.log(convertHashToArray({name: "Jeremy", age: 24, role: "Software Engineer"})) // [["age", 24], ["name", "Jeremy"], ["role", "Software Engineer"]]
+
+/*
+Gauss Theorem n(n+1)/2 but for the power of 3 per each element ((n*(n + 1))/2)**2
+Return the sum of 1 to the input integer with each element to the power of 3
+input n 1^3 + 2^3 + ... + n^3
+    2 => 9
+    3 => 36
+*/
+const sumCubes = (n) => ((n*(n + 1))/2)**2
+
+// Alternative Answer
+// const sumCubes = n => [...Array(n + 1).keys()].reduce((r, i) => r + i ** 3)
+
+console.log(sumCubes(2)) // 9
+console.log(sumCubes(3)) // 36
