ef6230a67b0ba7553839d23136635abd594ed758
KevinTapol <kevintapolcanyi@gmail.com>
2024-09-07 19:49:22 -0500

    JSON.stringify typeof() === 'object'

--- codewars343.js
+++ codewars343.js
@@ -21,11 +21,38 @@ Examples:
 
 Pseudocode:
     JSON.stringify() the input 
-    iterate through and grab every instance of ': ' to '}' that does not contain 'null' and push it to an empty array
+    iterate through and grab every instance of ': ' to '}' that does not contain 'null' and push it to an empty array using .slice(indexOf(':') + 2, indexOf('))
     return that array
+
+    alternate
+    create a array of object values and if the value is an object then create a 
 */
 
 // My Answer
-function listToArray(arr) {
-    //write your code here
+function listToArray(obj) {
+     let result = []
+     let arr = Object.values(obj)
+     while(typeof(e) === 'object'){
+
+     }
+    // const objCheck = n => typeof(n) === 'object' ? Object.values(n) : ''
+    // return arr.map(e => typeof(e) === 'object' ? objCheck(e) : result.push(e))
 }
+
+console.log(listToArray({value: 1, next: {value: 2, next: {value: 3, next: null}}})) //
+/*
+declare an empty array result to push values to
+if the current value type is a string boolean or an integer then push it to result
+if the current value type is an object then while loop until the value is not an object and push it to result?
+function strCount(obj){
+    let temp = 0;
+    JSON.stringify(obj,(key,value)=>{
+        if (typeof value === 'string') {
+            temp++
+        }
+        return value;
+    })
+
+    return temp
+}
+*/
