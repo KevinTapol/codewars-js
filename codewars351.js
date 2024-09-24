481accd34c3233a8826c9be04bb527d83a8b2786
KevinTapol <kevintapolcanyi@gmail.com>
2024-09-23 21:12:32 -0500

    unique array of duplicates [...new Set(arr.filter((e,i)=> i !== arr.indexOf(e)))]

--- codewars351.js
+++ codewars351.js
@@ -11,7 +11,7 @@ Parameters or Edge Cases:
     string representations of integers will not be considered duplicates of each other
 
 Return:
-    return an array of duplicates not counting the first appearance of the duplicates
+    return a unique array of elements that occur more than once from the input array while maintaining the order
 
 Examples:
     [1, 2, 4, 4, 3, 3, 1, 5, 3, "5"]  ==>  [4, 3, 1]
@@ -19,23 +19,101 @@ Examples:
     [] ==>  []
 
 Pseudocode:
-    declare an empty array named result
-    double for loop
-    iterate through the input array take the first element of the input array and compare it with the next till the end
-        if any of the compared elements are duplicates push it to the array result
-    return the array result
+    create a copy of the input array to be modified named result
+    create a copy of unique values from the input array named unique
+    iterate the length of the unique set and find the index of the first instance of the element from the array result and set the element equal to an empty string
+    create a unique set from the result array and return it
 */
 
 // My Answer
-function duplicates(arr) {
-    let result = []
-    for(let i = 0; i < arr.length; i++){
-        for(let j = i + 1; j < arr.length; j++){
-            if(arr[i] === arr[j]){
-                result.push(arr[j])
-            }
+function duplicates(arr) { 
+
+    let result = arr
+    let unique = [...new Set(arr)]
+
+    for (let i = 0; i < unique.length; i++) {
+        let index = result.findIndex(e => e === unique[i])
+
+        if (index !== -1) {
+            result[index] = ""
         }
-    }return result
+    }
+    return [...new Set(result.filter(e => e !== ""))]
 }
 
-console.log(duplicates([1, 2, 4, 4])) // [4]
+console.log(duplicates([1, 2, 4, 4, 3, 3, 1, 5, 3, "5"])) // [4, 3, 1]
+
+// IMO Best Practices and Most Clever
+/* 
+Filter the input array where the current index is not equal to the indexOf() the current element. This creates an array of duplicates removing the first occurrence of the duplicates. Then using [...new Set()] to create a unique array of the array of duplicates.
+*/
+const duplicates = arr => [...new Set(arr.filter((el, i) => i !== arr.indexOf(el)))];
+
+// Best Practices and Most Clever
+/* 
+    using inputArray.reduce((acc, c, input, array) =>  {
+    if the current index is not equal to the index of the current element c AND the current element does not exist in the accumulative array acc   then push the current element to the accumulative array acc
+    }, [])
+*/
+function duplicates(arr) {
+    return arr.reduce(function(prev, cur, i, a) {
+      if (i !== a.indexOf(cur) && prev.indexOf(cur) === -1) {
+        prev.push(cur);
+      }
+      return prev;
+    }, []);
+  }
+
+function duplicates(arr) {
+    return arr.reduce((prev, cur, i, a) => {
+      if (i !== a.indexOf(cur) && prev.indexOf(cur) === -1) {
+        prev.push(cur);
+      }
+      return prev;
+    }, []);
+}
+
+// using new Map() and new Set() 
+const  duplicates = arr => {
+    let map = new Map();
+    let set = new Set();
+    arr.forEach(e => {
+      map.get(e) ? set.add(e) : map.set(e, true)
+    })
+    return Array.from(set)
+}
+
+// using .forEach
+function duplicates(array) {
+    var result = [], counts = new Map();
+    array.forEach(function(x) {
+        var c = (counts.get(x) || 0) + 1;
+        if (c === 2)
+          result.push(x);
+        counts.set(x, c);
+      });
+    return result;
+}
+
+// using .filter() with indexOf() conditionals
+function duplicates(arr) {
+    return arr.filter((el, i) => i !== arr.indexOf(el) && i === arr.indexOf(el, arr.indexOf(el) + 1))
+};
+
+// using .includes()
+function duplicates(arr) {
+    let x = [];
+    let y = [];
+  
+    arr.forEach((i) => {
+        if (x.includes(i) === false) return x.push(i);
+        if (x.includes(i) && y.includes(i) === false) return y.push(i);
+    })
+  
+    return y
+}
+
+// very clever using indexOf(e) < i
+function duplicates(arr) {
+    return [...new Set(arr.filter((e,i)=>arr.indexOf(e)<i))]
+}
