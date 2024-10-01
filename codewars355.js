5ef7f267ce60f0affc3958736508a35b3ff977f1
KevinTapol <kevintapolcanyi@gmail.com>
2024-09-30 22:11:44 -0500

    [...obj].flat(Infinity) forcing multidimensional arrays or object inputs into a single 1d array

--- codewars355.js
+++ codewars355.js
@@ -2,8 +2,9 @@
 Naughty or Nice?
 
 Parameters or Edge Cases:
-    there can be more than 1 input but all inputs will be objects
-    inputs will be objects representing people key name with values of strings representing names and key wasNice with values of boolean true or false
+    inputs can be arrays, nested arrays or objects
+    there can be more than 1 input 
+    objects represent people key name with values of strings representing names and key wasNice with values of boolean true or false
     
 Return:
     return 2 different functions where getNiceNames returns an array of the object values of name as an array of string elements where wasNice has boolean true and getNaughtyNames where wasNice has boolean false
@@ -14,17 +15,18 @@ Examples:
 
   getNiceNames() ['Santa', 'Warrior reading this kata']
   getNaughtyNames() []
+
 Pseudocode:
-    create an array of all the input objects
+    create an array of all the input objects and flatten the array forcing all inputs whether objects or arrays to be a 1 dimensional array
     iterate through the array grabbing all elements with the key value wasNice: true (for getNice Names) :false (for getNaughtyNames) and push their key value names: string to the array result
     return the array result
 */
 
 // My Answer
 function getNiceNames(...obj){
-    
+    let flatArr = obj.flat(Infinity)
     let result = []
-    for(let e of obj){
+    for(let e of flatArr){
         if(e['wasNice'] === true){
             result.push(e['name'])
         }
@@ -34,8 +36,9 @@ function getNiceNames(...obj){
 
   
   function getNaughtyNames(...obj){
+    let flatArr = obj.flat(Infinity)
     let result = []
-    for(let e of obj){
+    for(let e of flatArr){
         if(e['wasNice'] === false){
             result.push(e['name'])
         }
