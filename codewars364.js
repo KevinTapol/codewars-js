33b693f2741bfa7c37051d4207cdf031f4be346a
KevinTapol <kevintapolcanyi@gmail.com>
2024-10-16 08:21:40 -0500

    .replace(regex, ternary fn) /[aeiou]/g

--- codewars364.js
+++ codewars364.js
@@ -17,6 +17,7 @@ Examples:
 Pseudocode:
     uppercase the string using regex and .replace() replace every 'a' or 'A' with '@' and every 'eiou' with '*'
     replace each white space with '!!!! '
+    finally concat '!!!!' to the end of the new string
     return the new string
 */
 
@@ -25,4 +26,66 @@ function gordon(a){
     return a.toUpperCase().replace(/[A]/gi,'@').replace(/[EIOU]/gi, '*').replace(/[ ]/gi, '!!!! ') + '!!!!'
 }
 
+// My Answer refactored one liner arrow fn
+const gordon = a => a.toUpperCase().replace(/[A]/gi,'@').replace(/[EIOU]/gi, '*').replace(/[ ]/gi, '!!!! ') + '!!!!'
+
 console.log(gordon('WhaAt feck damn cake')) // 'WH@T!!!! F*CK!!!! D@MN!!!! C@K*!!!!'
+
+// Best Practices and Most Clever
+function gordon(ramsay) {
+    return ramsay
+      .toUpperCase()
+      .replace(/\w+/g, '$&!!!!')
+      .replace(/[AEIOU]/g, v => v == 'A' ? '@' : '*');
+}
+
+// using .split() and .join() to replace ' ' with '!!!!' instead of replace(regex, fn)
+const gordon=a=>a.toUpperCase().split(" ").map(a=>a.replace(/A/g,"@").replace(/[EIOU]/g,"*")+"!!!!").join(" ");
+
+// brute force for loop with conditionals
+function gordon(a){
+    const vowels = 'AEIOU';
+    let result = '';
+    
+    for(let char of a) {
+      
+      if (vowels.includes(char.toUpperCase()) && char.toUpperCase() === 'A') {
+        result += '@';
+      } else if (vowels.includes(char.toUpperCase())) {
+        result += '*';
+      } else if (char === ' ') {
+        result += '!!!! ';
+      } else {
+        result += char.toUpperCase();
+      }
+    }
+    
+    result += '!!!!';
+    
+    return result;
+}
+
+// brute force for loop with switch case instead of if else conditionals
+function gordon(str){
+    let strUp = str.toUpperCase().split(' ')
+    for(let i = 0; i < strUp.length; i++) {
+      let letter = strUp[i].split('')
+      for(let j = 0; j < letter.length; j++) {
+        switch(letter[j]) {
+            case 'A':
+            letter[j] = '@';
+            break;
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+            letter[j] = '*';
+            break;
+        }
+      }
+      strUp[i] = letter.join('') + '!!!!'
+    }
+    
+    return strUp.join(' ') 
+    
+}
