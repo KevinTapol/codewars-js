3fcf11cebbfc3779d620841b77efe7add79c8797
KevinTapol <kevintapolcanyi@gmail.com>
2024-11-25 10:07:15 -0600

    str.replace(regex, fn) key reference obj arr str .indexOf() .charAt()

--- codewars398.js
+++ codewars398.js
@@ -37,23 +37,23 @@ Pseudocode:
 */
 
 // My Answer brute force for loop w/o regex
-// const key = '0aeiou'
+const key = '0aeiou'
 
-// function encode(str) {
-//     let result = ''
+function encode(str) {
+    let result = ''
 
-//     for(let e of str){
-//         key.includes(e) === true ? result += key.indexOf(e) : result += e
-//     } return result
-// }
+    for(let e of str){
+        key.includes(e) === true ? result += key.indexOf(e) : result += e
+    } return result
+}
   
-//   function decode(str) {
-//     let result = ''
+  function decode(str) {
+    let result = ''
     
-//     for(let e of str){
-//         '012345'.includes(e) === true ? result += key[Number(e)] : result += e
-//     } return result
-// }
+    for(let e of str){
+        '012345'.includes(e) === true ? result += key[Number(e)] : result += e
+    } return result
+}
 
 // My Answer using regex
 function encode(str) {
@@ -84,3 +84,45 @@ function encode(string){
 function decode(string){
     return string.replace(/[1-5]/g, function (x) { return '_aeiou'.charAt(x) });
 }
+
+// Clever use of declared array and index referencing with array methods || or current element logic
+const table = ['a', 'e', 'i', 'o', 'u']
+const encode = string => string.split('').map(x => table.indexOf(x) + 1 || x).join('')
+const decode = string => string.split('').map(x => table[Number(x) - 1] || x).join('')
+
+// using object for key instead of array or str
+function encode(string){
+    var vowelMapping = {'a': 1, 'e': 2, 'i': 3, 'o': 4, 'u': 5};
+    return codeStringGivenMapping(string, vowelMapping);
+  }
+  
+  function decode(string){
+    var vowelMapping = {'1': 'a', '2': 'e', '3': 'i', '4': 'o', '5': 'u'};
+    return codeStringGivenMapping(string, vowelMapping); 
+  }
+  
+  function codeStringGivenMapping(string, mapping){
+    return string.split('').map(function(char){
+      return mapping[char] || char;
+    }).join(''); 
+  }
+
+// refactored version of object
+const obj = {a: 1, e: 2, i: 3, o: 4, u: 5};
+
+const encode = string =>
+  string.replace(/[aeiou]/g, val => obj[val]);
+
+const decode = string =>
+  string.replace(/[1-5]/g, val => Object.keys(obj)[--val]);
+
+/*
+.bind() method of Function instances creates a new function that, when called, calls this function with its this keyword set to the provided value, and a given sequence of arguments preceding any provided when the new function is called.
+*/   
+  var encode = curry.bind(/[aeiou]/g, function(f){ return 'aeiou'.search(f) + 1 });
+  var decode = curry.bind(/[1-5]/g, function(f){ return 'aeiou'[--f] });
+  function curry(f, s){ return s.replace(this, f) }
+
+// the reason this doesn't fail on whitespace by removing whitespace for decode is due to the regex
+const encode = s => s.replace(/[aeiou]/g, v => ' aeiou'.indexOf(v));
+const decode = s => s.replace(/\d/g, v => ' aeiou'[v]);
