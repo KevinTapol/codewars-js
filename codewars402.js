3ce7e19ab849fa08539ec4057ec70fb2d61e1698
KevinTapol <kevintapolcanyi@gmail.com>
2024-12-01 08:15:45 -0600

    nested for loop vs nested .map() sum matrix

--- codewars402.js
+++ codewars402.js
@@ -59,23 +59,62 @@ function matrixAddition(a, b){
     return result
 }
 
-// 
-function matrixAddition(a, b){
-    let result = a
-
-    for(let i = 0; i < a.length; i++){
-        for(let j = 0; j < a[i].length; j++){
-            result[i][j] = a[i][j] + b[i][j]
-        }
-    }
-    return result
+// My Answer using nested .map() instead of nested for loops
+function matrixAddition(a, b, z = a){
+    return z.map((e, i) => e.map((el, j) => el = a[i][j] + b[i][j]))
 }
+
+// Ny Answer refactored one liner arrow fn array methods
+const matrixAddition = (a, b, z = a) => z.map((e, i) => e.map((el, j) => el = a[i][j] + b[i][j]))
 
 console.log(matrixAddition( [ [1, 2, 3], [3, 2, 1], [1, 1, 1] ], [ [2, 2, 1], [6, 4, 4], [2, 2, 4] ] ))
 
-// let a = [ [1, 2, 3], [3, 2, 1], [1, 1, 1] ]
-// let b = [ [2, 2, 1], [6, 4, 4], [2, 2, 4] ]
-// let c = a[1][0] + b[1][0]
-// let z = [[0,0,0],[0,0,0],[0,0,0]]
-// z[1][0] = c
-// console.log(z)
+// Best Practices and Most Clever
+function matrixAddition(a, b){
+    return a.map(function(n, i){
+      return n.map(function(o, j){
+        return o + b[i][j];
+      });
+    });
+}
+
+// Best Practices and Most Clever refactored using arrow fn
+// similar to my nested .map() but since they are referencing a for the input the inner nested current element of o is equivalent to a[i][j]
+function matrixAddition(a, b){
+    return a.map((n, i) => n.map((o, j) => o + b[i][j]))
+}
+
+// same as Best Practices and Most Clever but using row and column to reference current element which is great for a mental picture of the matrix
+function matrixAddition(a, b){
+    return a.map(function(row, i) {
+      return row.map(function(col, ii) {
+        return col + b[i][ii];
+      });
+    });
+}
+
+// same idea with great mental picture declared variables of nested .map()
+const matrixAddition = (a, b) => a.map((row, rowIndex) => row.map((value, columnIndex) => value + b[rowIndex][columnIndex]));
+
+// nested for loop except they are using an empty array local for loop assign the sums to instead of copying the structure of one of the inputs
+function matrixAddition(a, b){
+    var c = [];
+    for(var n = 0; n < a.length; n++){
+      c[n] = [];
+      for(var m = 0; m < a[n].length; m++){
+        c[n][m] = a[n][m] + b[n][m];
+      }
+    }
+    return c;
+  }
+
+// recursion
+function matrixAddition(a, b){
+    var c = [];
+  
+    for (var i = 0; i < a.length; i++) {
+      c[i] = (typeof a[i] !== 'number') ? matrixAddition(a[i], b[i]) : a[i] + b[i];
+    }
+    
+    return c;
+}
