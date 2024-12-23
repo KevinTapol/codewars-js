7e12dbdaa8c4069b4a4f7015d68f2c4760c85bfb
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2024-12-22 18:25:33 -0600

    correct answer using brute force for loop but consider using  .map() with .every() for implicit boolean return

---
+++ codewars407.js
@@ -0,0 +1,60 @@
+/*
+Possibilities Array
+A non-empty array a of length n is called an array of all possibilities if it contains all numbers between 0 and a.length - 1 (both inclusive).
+
+Write a function that accepts an integer array and returns true if the array is an array of all possibilities, else false.
+
+Examples:
+
+[1,2,0,3] => True
+# Includes all numbers between 0 and a.length - 1 (4 - 1 = 3)
+
+[0,1,2,2,3] => False
+# Doesn't include all numbers between 0 and a.length - 1 (5 - 1 = 4)
+
+[0] => True
+# Includes all numbers between 0 and a.length - 1 (1 - 1 = 0).
+
+Parameters or Edge Cases:
+    inputs will be a non empty array of integers
+
+Return:
+    return boolean true if a generated array from 0 to the input array length -1 every element exists in the input array else false
+
+Examples:
+    [1,2,0,3] --> true
+    [0,1,2,2,3] --> false
+    [0] --> true
+    [0,1,2,3] --> true
+    [1,2,3,4] --> false
+
+Pseudocode:
+    create an array named check that consists of integer elements starting from 0 to the input array length
+    iterate through the array check 
+        if the current element of check does not exist in the input array return false
+    outside of the iteration return true
+
+*/
+
+// My Answer brute force for loop
+function isAllPossibilities(x){
+    const check = Array.from({length:x.length}, (e, i) => e = i)
+    for(let e of check){
+        if(!x.includes(e)){
+            return false
+        }
+    }return true
+}
+
+// My Answer refactored
+function isAllPossibilities(x){
+    const check = Array.from({length:x.length}, (e, i) => e = i)
+    for(let e of check){
+        if(!x.includes(e)){
+            return false
+        }
+    }return true
+}
+
+console.log(isAllPossibilities([1,2,0,3])) // true 
+console.log(isAllPossibilities([0,1,2,2,3])) // false
