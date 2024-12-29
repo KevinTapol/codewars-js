6363c38e31a28f30130b75d23807b91e9b95b3e0
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2024-12-28 19:35:50 -0600

    1 of 3 issues discovered considering stringify and regex to test multiple character integrity

--- codewars409.js
+++ codewars409.js
@@ -42,3 +42,20 @@ function replaceAll(seq, find, replace) {
 // console.log(replaceAll([1,2,2], 1, 2)) // [2,2,2]
 // console.log(replaceAll([ 1, 1, 2 ], 1, 2)) // [ 2, 2, 2 ]
 console.log(replaceAll('Hello World', 'o', '0')) // 'Hell0 W0rld'
+
+/*
+Test if each digit existing is the correct answer or maintain double digit integer elements
+Consider converting the entire first input into a string then use regex to replace every instance like 12 replace 1 with 3 and get 32 or maintain 12?
+[
+  12,  7,  9,  2,  8,  5, 15,  6,  9, 16,  2, 12,
+   3, 19,  1,  8, 18, 12,  6, 12, 14,  2, 11, 10,
+   2, 13,  2, 11, 11,  3,  1, 17, 19, 20,  4,  1,
+  10, 10, 11, 20,  9, 10,  6, 13, 10, 11, 12,  4,
+   5,  9,  5,  4, 11, 11, 17,  5,  1,  5, 15,  3,
+  16, 18,  8,  4,  9, 15, 16, 15,  3,  6, 19, 17,
+  12,  1,  5, 17,  6,  9,  4,  8, 18, 11,  1
+]
+9
+5
+
+*/
