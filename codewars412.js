64c340503f11d47da85bd5887144b62c28620ccf
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2025-01-09 18:58:28 -0600

    compare concatenated arrays vs unique set concatenated arrays working consider iteration if element exists in both

---
+++ codewars412.js
@@ -0,0 +1,29 @@
+/*
+Array comparator
+You have two arrays in this kata, every array contains unique elements only. Your task is to calculate number of elements in the first array which are also present in the second array.
+
+Parameters or Edge Cases:
+    both inputs will be arrays containing random unique elements including strings and integers
+
+Return:
+    return the count of elements that exist in both array inputs
+
+Examples:
+    ['Perl','Closure','JavaScript'], ['Go', 'C++','Erlang'] --> 0
+    ['incapsulation','OOP','array'], ['time', 'propert','paralelism','OOP'] --> 1
+    [1,2,3,4,5], [2,3,4,5,6] --> 4
+
+Pseudocode:
+    concat the input arrays into one array named inputs
+    create a unique set from the concatenated array inputs named unique
+    return the difference of the length inputs and unique
+*/
+
+// My Answer
+function matchArrays(v,r){
+    const inputs = v.concat(r)
+    const unique = [...new Set(inputs)]
+    return inputs.length - unique.length
+}
+
+console.log(matchArrays(['incapsulation','OOP','array'], ['time', 'propert','paralelism','OOP'])) // 1
