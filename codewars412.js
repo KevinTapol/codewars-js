45353a392f0f3d4f33314f8ea587304444c655d2
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2025-01-11 17:05:18 -0600

    arr1.filter(e=>arr2.indexOf(e)!=-1).length swap indexOf() with .includes() vs .reduce()

--- codewars412.js
+++ codewars412.js
@@ -35,3 +35,25 @@ function matchArrays(v,r, arr = v.concat(r)){
 const matchArrays = (v,r, arr = v.concat(r)) => arr.length - [...new Set(arr)].length
 
 console.log(matchArrays(['incapsulation','OOP','array'], ['time', 'propert','paralelism','OOP'])) // 1
+
+// Best Practices and Most Clever
+// very clever to check if there is an index where the current element from the array v that exists in the array r by using not equal to -1 meaning not not exist
+const matchArrays = (v, r) => v.filter(e => r.indexOf(e) != -1).length;
+
+// Brute force nested for loop
+function matchArrays(v,r){
+    var i, j, result = 0;
+    for (i = 0; i < v.length; i++) {
+      for (j = 0; j < r.length; j++) {
+        if (v[i] === r[j])
+          result++;
+      }
+    }
+    return result;
+}
+
+// using .filter() .includes() and .length
+const matchArrays = (a, b) => a.filter(e => b.includes(e)).length;
+
+// using .reduce on one array to count the elements included in the 2nd array
+const matchArrays = (v,r) => v.reduce((pre, val) => pre + r.includes(val), 0);
